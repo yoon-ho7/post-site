a7f65fa39e51ba2535b2ce7964fdfbb958c619cb
yoon-ho7 <suho0317@gmail.com>
2021-11-25 17:31:28 +0900

    [ADD]게시판 만들기

--- post.js
+++ post.js
@@ -1,9 +1,24 @@
+const write = document.querySelector('write');
+
 function open() {
     document.querySelector('#modal').style.display = 'flex';
 }
 function close() {
-    document.querySelector('#modal').style.display = 'none'
+    document.querySelector('#modal').style.display = 'none';
+}
+
+function change() {
+    document.querySelector('#modal').style.display = 'none';
+}
+function remove() {
+    document.querySelector('#modal').style.display = 'none';
 }
 
 document.querySelector("#pWrite").addEventListener("click", open)
 document.querySelector("#modal").addEventListener("click", close)
+document.querySelector('#writeBox').addEventListener("click", function(e){
+  e.stopPropagation();
+});
+document.querySelector('#write').addEventListener("click", close)
+document.querySelector('#revise').addEventListener("click", change)
+document.querySelector('#delete').addEventListener("click", remove)
