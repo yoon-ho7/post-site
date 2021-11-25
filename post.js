d50bbdc8503cf21d7753e8948c88678ea7a770cf
yoon-ho7 <suho0317@gmail.com>
2021-11-25 23:32:16 +0900

    [FEAT] JS 수정 및 보완

--- post.js
+++ post.js
@@ -1,24 +1,43 @@
-const write = document.querySelector('write');
+const write = document.querySelector('#write');
+const revise = document.querySelector("#revise");
+const del = document.querySelector('#delete');
+const modal = document.querySelector('#modal');
+const post = document.querySelector('.post');
 
 function open() {
-    document.querySelector('#modal').style.display = 'flex';
+    modal.style.display = 'flex';
+    write.style.display = 'flex';
+    revise.style.display = 'none';
+    del.style.display = 'none';
 }
 function close() {
-    document.querySelector('#modal').style.display = 'none';
+    modal.style.display = 'none';
 }
-
 function change() {
-    document.querySelector('#modal').style.display = 'none';
+    modal.style.display = 'flex';
+    write.style.display = 'none';
+    revise.style.display = 'flex';
+    del.style.display = 'flex';
 }
 function remove() {
-    document.querySelector('#modal').style.display = 'none';
+    modal.style.display = 'none';
 }
 
 document.querySelector("#pWrite").addEventListener("click", open)
-document.querySelector("#modal").addEventListener("click", close)
 document.querySelector('#writeBox').addEventListener("click", function(e){
   e.stopPropagation();
 });
-document.querySelector('#write').addEventListener("click", close)
-document.querySelector('#revise').addEventListener("click", change)
-document.querySelector('#delete').addEventListener("click", remove)
+modal.addEventListener("click", close)
+revise.addEventListener("click", close)
+del.addEventListener("click", close)
+post.addEventListener("click", change)
+
+write.onclick = function () {
+    close();
+}
+revise.onclick = function () {
+    alert('수정되었습니다.');
+}
+del.onclick = function () {
+    alert('삭제되었습니다.');
+}
